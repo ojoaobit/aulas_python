1bc597b59924fc3410e395b76681d80339a43759
ojoaobit <victorbatistame@gmail.com>
2024-08-31 13:51:47 -0400

    initial commit

---
+++ atividade.py
@@ -0,0 +1,33 @@
+# DIGITE O NUMERO E IMPRIMA ELE
+'''
+numero = int(input("DIGITE UM NUMERO INTEIRO: "))
+print(f"NUMERO DIGITADO FOI: {numero}")
+'''
+
+# SOMANDO 2 VALORES
+'''
+num1 = int(input("DIGITE O PRIMEIRO VALOR: "))
+num2 = int(input("DIGITE O PRIMEIRO VALOR: "))
+soma = num1 + num2
+
+print(f"A SOMA DOS VALORES E: {soma}")
+'''
+
+# CALCULA AREA DO QUADRADO E MOSTRA O DOBRO DA AREA
+'''
+lado = float(input("DIGITE O VALOR DO LADO DO QUADRADO: "))
+area = lado ** 2
+dobro = 2 * area
+
+print(f"AREA DO QUADRADO: {area}")
+print(f"O DOBRO DA AREA DO QUADRADO: {dobro}")
+'''
+
+# CONVERTENDO FAHRENHEIT PRA CELSIUS
+'''
+fahrenheit = float(input("DIGITE A TEMPERATURA EM °F: "))
+conversao = 5 * ((fahrenheit-32) / 9)
+
+print(f"A TEMPERATURA EM CELSIUS É: {conversao:.1f} °C")
+'''
+
